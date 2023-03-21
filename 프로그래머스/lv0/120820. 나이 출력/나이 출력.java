caca9cecf39671f2cce74e2420fbff6c8aab69bc
leesfact <121993153+leesfact@users.noreply.github.com>
2023-03-22 07:57:40 +0900

    [level 0] Title: 나이 출력, Time: 0.04 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        int year = 2022;
+        
+        answer = year -age + 1;
+        
+        return answer;
+    }
+}
