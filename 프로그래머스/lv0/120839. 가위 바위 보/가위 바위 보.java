b6339922b975ac87463c1b8d23fe3240ec114de4
leesfact <121993153+leesfact@users.noreply.github.com>
2023-03-22 08:37:04 +0900

    [level 0] Title: 가위 바위 보, Time: 2.01 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,18 +1,15 @@
 class Solution {
     public String solution(String rsp) {
         String answer = "";
-	        StringBuilder sb = new StringBuilder();
-	        
 	        for(int i = 0; i < rsp.length(); i++) {
 	        	if(rsp.charAt(i)=='5') {
-	        		sb.append('2');
+	        		answer +='2';
 	        	}else if(rsp.charAt(i) == '0') {
-	        		sb.append('5');
+	        		answer +='5';
 	        	}else {
-	        		sb.append('0');
+	        		answer +='0';
 	        	}
 	        }
-	        answer = sb.toString();
 	        return answer;
     }
 }
