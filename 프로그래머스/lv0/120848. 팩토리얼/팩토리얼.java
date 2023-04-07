4a46ba1edb4e4563be76901810e23c9d2f18b8c1
leesfact <121993153+leesfact@users.noreply.github.com>
2023-04-07 15:01:44 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
@@ -1,14 +1,13 @@
 class Solution {
     public int solution(int n) {
-       int answer = 0;
-		int[] factor = {1,2,6,24,120,720,5040,40320,362880,3628800};
-		
-		for(int i = 0; i < 10;  i++) {
-			if(factor[i] <= n) {
-				answer= i+1;
-			}
-		}
+      int i = 1;
+        int factorial = 1;
 
-        return answer;
+        while (factorial <= n) {
+            i++;
+            factorial *= i;
+        }
+
+        return i - 1;
     }
 }
