d93ba8f7fff84d2950964df618b487f0e1174afe
leesfact <121993153+leesfact@users.noreply.github.com>
2023-04-30 21:45:36 +0900

    [level 0] Title: 유한소수 판별하기, Time: 6.14 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -1,24 +1,33 @@
 class Solution {
     public int solution(int a, int b) {
-            int answer = 0;
-    if (a == 1) return 1;
-    
-    // 소인수분해
-    for (int i = 2; i <= a; i++) {
-        if (a % i == 0 && b % i == 0) {
-            a /= i;
-            b /= i;
-            i--;
-        }
-    }
-    
-    // 2와 5 이외의 소수가 있는지 확인
-    while (b % 2 == 0) b /= 2;
-    while (b % 5 == 0) b /= 5;
-    
-    if (b == 1) answer = 1;
-    else answer = 2;
-    
-    return answer;
+          int answer = 0;
+		 	if (a == 1) return 1;
+	        for(int i = 2; i <= a; i++) {
+	        	if (a % i == 0) {
+	        		if(a % i == 0 && b % i == 0) {
+	        			a /= i;
+	        			b /= i;	
+	        			
+	        		}
+	        	}	
+	        }
+	        System.out.println("a: " + a);
+	        System.out.println("b: " + b);
+	        
+	        if(b ==1 ) answer = 1;
+	        while (b != 1) {
+	        	if(b % 2 == 0) {
+	        		b /= 2;
+	        	}else if( b % 5 ==0) {
+	        		b  /= 5;
+	        	}else {
+	        		answer = 2;
+	        		break;
+	        	}
+	        	answer =  1;
+	        }
+	       // System.out.println(b);
+	        System.out.println(answer);
+	        return answer;
     }
 }
