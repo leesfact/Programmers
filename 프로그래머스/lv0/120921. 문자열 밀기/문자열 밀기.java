727610f17f072648592d707518b36afdadfaca73
leesfact <121993153+leesfact@users.noreply.github.com>
2023-05-05 23:09:34 +0900

    [level 0] Title: 문자열 밀기, Time: 1.40 ms, Memory: 90.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(String A, String B) {
+       int answer = -1;
+        String[] strArray = new String[A.length()];
+        int length = A.length();
+        if(A.equals(B)) {
+        	return 0;
+        }else {
+        	A +=A;
+        	for(int i = 0; i < length; i++ ) {
+        		strArray[i] = A.substring(i,i+length);
+        		if(strArray[i].equals(B)) {
+        			answer = Math.abs(i-length);
+                
+        		}
+        	}
+        }
+        
+        return answer;
+    }
+}
