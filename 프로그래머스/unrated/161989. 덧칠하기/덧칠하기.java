6bae729c4e0af233a497af088df2aab7198919b1
leesfact <121993153+leesfact@users.noreply.github.com>
2023-05-29 22:57:49 +0900

    [unrated] Title: 덧칠하기, Time: 0.95 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int roller = section[0];
+        int cnt = 1;
+        for(int i = 1; i < section.length; i++) {
+        	
+            if(roller + m - 1 < section[i]) {
+                cnt++;
+                roller = section[i];
+            }
+        }
+        return cnt;
+    }
+}
