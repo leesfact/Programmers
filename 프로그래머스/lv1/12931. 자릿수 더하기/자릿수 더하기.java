0b1909c964727490b6512240e859dab0601fc2b2
leesfact <121993153+leesfact@users.noreply.github.com>
2023-06-05 07:58:57 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.13 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+    int answer = 0;
+        String str = Integer.toString(n);
+     
+       
+        for(int i = 0; i < str.length(); i++) {
+        	answer += Integer.parseInt(str.split("")[i]);
+        }
+        
+        
+
+        return answer;
+    }
+}
