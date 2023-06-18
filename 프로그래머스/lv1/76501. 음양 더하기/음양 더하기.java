812fec05e1491d12f7055058d6ef129323ea4313
leesfact <121993153+leesfact@users.noreply.github.com>
2023-06-18 21:37:27 +0900

    [level 1] Title: 음양 더하기, Time: 0.04 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -2,16 +2,10 @@ class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
         
-        for(int i = 0; i < absolutes.length; i++) {
-        	boolean isFlag = (signs[i] == false) ? true : false;
-        	
-        	if(isFlag) {
-        		answer -= absolutes[i];
-        	}else {
-        		answer += absolutes[i];
-        	}
+        for (int i = 0; i < absolutes.length; i++) {
+            answer += signs[i] ? absolutes[i] : -absolutes[i];
         }
-     
+        
         return answer;
     }
 }
