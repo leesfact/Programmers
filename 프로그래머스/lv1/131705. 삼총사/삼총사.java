2512ee893602dc6deee301bfe4731b27fa954993
leesfact <121993153+leesfact@users.noreply.github.com>
2023-07-07 13:02:25 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -1,22 +1,23 @@
-import java.util.*;
+
 
 class Solution {
     public int solution(int[] number) {
-        int sum = 0;
-        int count = 0;
-
-        for (int i=0; i<number.length; i++) {
-            for ( int j=i+1; j<number.length; j++) {
-                for ( int k=j+1; k<number.length; k++) {
-                    sum += number[i] + number[j] + number[k];
-                    if (sum == 0) {
-                        count++;
-                    }
-                    sum = 0;
-                }
-            }
+          int cnt = 0;
+        
+        
+        for(int i = 0; i < number.length; i++) {
+        	for(int j = i+1; j < number.length; j++) {
+        		for(int k = j+1; k < number.length; k++) {
+      
+        			if( number[i] + number[j] + number[k] == 0) {
+        				cnt++;
+        			}
+        		}
+        	}
         }
-
-        return count;
+        
+        
+    
+        return cnt;
     }
 }
