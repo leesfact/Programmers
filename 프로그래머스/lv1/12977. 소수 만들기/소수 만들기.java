6eb17ebce3bcce37ce0c3da4d519ad866d71f615
leesfact <121993153+leesfact@users.noreply.github.com>
2023-07-22 12:07:29 +0900

    [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -2,51 +2,33 @@ class Solution {
     public static int solution(int[] nums) {
 	       
 		 
-	        return combination(nums, new boolean[nums.length],0, nums.length, 3);
-	 }
-	 
-	 
-	 public static int combination(int[] arr, boolean[] visited, int start, int n, int r) {
-		 int cnt = 0;
+	        	 int answer = 0;
 		 
-		 if(r == 0) {
-			 return checkPrime(arr, visited, n) ? 1: 0;
-		 }
 		 
-		 for(int i = start; i < n; i++) {
-			 visited[i] = true;
-			 
-			 cnt += combination(arr, visited, i+1, n, r-1);
-			 visited[i] = false;
-		 }
-		 
-		 return cnt;
-		 
-	 }
-	 
-	 
-	 public static boolean checkPrime(int[] arr, boolean[] visited, int n) {
-		 
-		 int sum = 0;
-		 
-		 for(int i= 0; i < n; i++) {
-			 if(visited[i]) {
-				 sum +=arr[i];
+		 for(int i = 0; i < nums.length -2; i++) {
+			 for(int j = i + 1; j < nums.length - 1; j++) {
+				 for(int k = j + 1; k < nums.length; k++) {
+					 
+					 if(isPrime(nums[i] + nums[j] + nums[k])) {
+						 answer += 1;
+					 }
+					 
+				 }
 			 }
 		 }
-		 return isPrime(sum);
+	        
+	 
+	
+		 return answer ;
 	 }
 	 
-	 public static boolean isPrime(int num) {
-		 if(num < 2) return false;
-		 
-		 for(int i = 2; i * i <= num; i++) {
-			 
-			 if(num % i == 0) {
-				 return false;
-			 }
-		 }
-		 
-		 return true;
-	 }
+	 public static Boolean isPrime(int num) {
+		    if (num < 2) return false;
+		    for(int i = 2; i <= (int)Math.sqrt(num); i++) {
+		        if(num % i == 0) {
+		            return false;
+		        }
+		    }
+		    return true;
+		}
 }
