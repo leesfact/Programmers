5352af64be6bc5213f92bffe633bc9e4fa3249d5
leesfact <121993153+leesfact@users.noreply.github.com>
2023-07-25 21:53:20 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 68.81 ms, Memory: 95 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+      Map<String, Integer> map = new HashMap<>();
+		
+		for(String player : participant) {
+			map.put(player, map.getOrDefault(player, 0) + 1);
+		}
+		
+		
+		for(String player : completion) {
+			map.put(player, map.get(player) -1);
+		}
+	
+		for (String key : map.keySet()) {
+            if (map.get(key) != 0){
+                return key;
+            }
+        }
+
+        return "";
+    }
+}
