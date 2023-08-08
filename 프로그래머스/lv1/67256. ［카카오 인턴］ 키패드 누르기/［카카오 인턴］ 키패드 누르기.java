9887156e26aeb3321af2f60e060f419c8e895838
leesfact <121993153+leesfact@users.noreply.github.com>
2023-08-08 15:23:43 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 3.14 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,39 +1,46 @@
 class Solution {
     public String solution(int[] numbers, String hand) {
         String answer = "";
-        int left = 10;
-        int right = 12;
-        
-        for(int number: numbers){
-            if(number == 1||number == 4||number == 7){
+		
+		int[] left = {3,0}; // *의 위치
+		int[] right = {3,2}; //#의 위치
+		
+		int[][] keyPad = {
+			{0, 0}, {0, 1}, {0, 2},
+            {1, 0}, {1, 1}, {1, 2},
+            {2, 0}, {2, 1}, {2, 2},
+            {3, 0}, {3, 1}, {3, 2}
+		};
+		for(int number: numbers) {
+            if(number == 1 || number == 4 || number == 7) {
                 answer += "L";
-                left = number;
-            }else if(number == 3||number == 6||number == 9){
+                left = keyPad[number - 1];
+            } else if(number == 3 || number == 6 || number == 9) {
                 answer += "R";
-                right = number;
-            }else {
-                if(number == 0) number=11;
-                int leftdist = Math.abs(number-left)/3+Math.abs(number-left)%3;
-                int rightdist = Math.abs(number-right)/3+Math.abs(number-right)%3;
-                
-                if(leftdist<rightdist){
-                    answer+="L";
-                    left = number;
-                }else if(leftdist>rightdist){
-                    answer+="R";
-                    right = number;
-                }else{
-                    if(hand.equals("left")){
-                        answer+="L";
-                        left = number;
-                    }else{
-                        answer+="R";
-                        right = number;
+                right = keyPad[number - 1];
+            } else {
+                if(number == 0) number = 11;
+                int leftDist = Math.abs(left[0] - keyPad[number-1][0]) + Math.abs(left[1] - keyPad[number-1][1]);
+                int rightDist = Math.abs(right[0] - keyPad[number-1][0]) + Math.abs(right[1] - keyPad[number-1][1]);
+
+                if(leftDist < rightDist) {
+                    answer += "L";
+                    left = keyPad[number - 1];
+                } else if(leftDist > rightDist) {
+                    answer += "R";
+                    right = keyPad[number - 1];
+                } else {
+                    if(hand.equals("left")) {
+                        answer += "L";
+                        left = keyPad[number - 1];
+                    } else {
+                        answer += "R";
+                        right = keyPad[number - 1];
                     }
                 }
             }
         }
+		
         return answer;
     }
-    
 }
