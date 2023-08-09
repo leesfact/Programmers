f573219e5140e0c5d7b6e0ffcc997f3b6fde82c5
leesfact <121993153+leesfact@users.noreply.github.com>
2023-08-09 18:13:32 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.24 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String solution(String new_id) {
+        new_id = new_id.toLowerCase();
+        
+        new_id = new_id.replaceAll("[^a-z0-9-_.]", "");
+        
+        new_id = new_id.replaceAll("\\.{2,}", ".");
+        
+        if(new_id.startsWith(".")) {
+        	new_id = new_id.substring(1);
+        }
+        
+        if(new_id.endsWith(".")) {
+        	new_id = new_id.substring(0, new_id.length()-1);
+        }
+        
+        if(new_id.isEmpty()) {
+        	new_id = "a";
+        }
+        
+        if(new_id.length() >= 16) {
+        	new_id = new_id.substring(0,15);
+        	if(new_id.endsWith(".")) {
+        		new_id = new_id.substring(0,new_id.length() -1);
+        	}
+        }
+        
+        if(new_id.length() <= 2) {
+        	while(new_id.length() < 3) {
+        		new_id += new_id.charAt(new_id.length() - 1);
+        	}
+        	
+        }
+        
+      
+        return new_id;
+    }
+}
