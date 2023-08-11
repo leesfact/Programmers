cb23843950fc3aea3fb9b1c2cecec44893ccb811
leesfact <121993153+leesfact@users.noreply.github.com>
2023-08-11 18:47:18 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 2.52 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,32 +1,45 @@
+import java.util.*;
 class Solution {
-    public String solution(String[] survey, int[] choices) {
-        int[] scores = new int[8];
-        int[] points = {3,2,1,0,1,2,3};
+    public String solution(String[] surveys, int[] choices) {
+        String answer = "";
+		HashMap<Character, Integer> map = new HashMap<>();
+		
+		char[] possibleChars = {'R', 'T', 'C', 'F', 'J', 'M', 'A', 'N'};
+		for (char ch : possibleChars) {
+		    map.put(ch, 0);
+		}
+		
+		
+		for(int i = 0; i < surveys.length; i++) {
+			if(choices[i] > 4) {
+				map.put(surveys[i].charAt(1), map.get(surveys[i].charAt(1)) + choices[i] - 4);
+			}else {
+				map.put(surveys[i].charAt(0), map.get(surveys[i].charAt(0)) + 4 - choices[i]);
+			}
+		}
+		
+		
+		if (map.get('R') >= map.get('T'))
+            answer += "R";
+        else
+            answer += "T";
+
+        if (map.get('C') >= map.get('F'))
+            answer += "C";
+        else
+            answer += "F";
+
+        if (map.get('J') >= map.get('M'))
+            answer += "J";
+        else
+            answer += "M";
+
+        if (map.get('A') >= map.get('N'))
+            answer += "A";
+        else
+            answer += "N";
         
-        for(int i = 0; i < survey.length; i++) {
-        	char[] pair = survey[i].toCharArray();
-        	int index = -1;
-        	
-        	switch (pair[choices[i] <= 4 ? 0 : 1]) {
-	        	case 'R': index = 0; break;
-	            case 'T': index = 1; break;
-	            case 'C': index = 2; break;
-	            case 'F': index = 3; break;
-	            case 'J': index = 4; break;
-	            case 'M': index = 5; break;
-	            case 'A': index = 6; break;
-	            case 'N': index = 7; break;
-        	}
-        	
-        	scores[index] += points[choices[i] - 1];
-        	
-        }
-        StringBuilder result = new StringBuilder();
-        result.append(scores[0] > scores[1] ? 'R' : scores[0] < scores[1] ? 'T' : 'R');
-        result.append(scores[2] > scores[3] ? 'C' : scores[2] < scores[3] ? 'F' : 'C');
-        result.append(scores[4] > scores[5] ? 'J' : scores[4] < scores[5] ? 'M' : 'J');
-        result.append(scores[6] > scores[7] ? 'A' : scores[6] < scores[7] ? 'N' : 'A');
         
-        return result.toString();
+        return answer;
     }
 }
