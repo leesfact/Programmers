48813294731c2a5fa3a8dd39eafaaec77424ea93
leesfact <121993153+leesfact@users.noreply.github.com>
2023-08-12 22:07:05 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.18 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] solution(String[] wallpaper) {
+       int minX = Integer.MAX_VALUE, minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE, maxY = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < wallpaper.length; i++) {
+            for (int j = 0; j < wallpaper[i].length(); j++) {
+                if (wallpaper[i].charAt(j) == '#') {
+                    if (i < minX) minX = i;
+                    if (i > maxX) maxX = i;
+                    if (j < minY) minY = j;
+                    if (j > maxY) maxY = j;
+                }
+            }
+        }
+
+        maxX += 1;  // Here
+        maxY += 1;  // And here
+        
+        
+        return new int[]{minX, minY, maxX, maxY};
+    }
+}
