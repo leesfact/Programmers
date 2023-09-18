2b1e64b9b915a511cded4882e8f44df1b936ef02
leesfact <121993153+leesfact@users.noreply.github.com>
2023-09-18 22:13:17 +0900

    [level 2] Title: [3차] n진수 게임, Time: 8.36 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,31 +1,21 @@
 class Solution {
     public String solution(int n, int t, int m, int p) {
-        StringBuilder result = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 		
-		String chars = "0123456789ABCDEF";
-		
-		int number = 0;
-		
-		while(result.length() <  m * t){
-			int currentNumber = number++;
-			StringBuilder temp = new StringBuilder();
-			
-			
-			do {
-				temp.insert(0, chars.charAt(currentNumber % n));
-				currentNumber /= n;
-			}while(currentNumber > 0); 
-			
-			result.append(temp);
-			
+		for(int i = 0; i <= t*m; i++) {
+			sb.append(Integer.toString(i, n).toUpperCase());
 		}
 		
-		StringBuilder tubeResult = new StringBuilder();
+		StringBuilder result = new StringBuilder();
 		
-		for(int i = 0; i < t; i++) {
-			tubeResult.append(result.charAt(p - 1 + m * i));
+		int i = p - 1;
+		for(int j = 0; j < t; j++) {
+		    result.append(sb.charAt(i));
+		    i += m;
 		}
 		
-        return tubeResult.toString();
+		
+		
+        return result.toString();
     }
 }
