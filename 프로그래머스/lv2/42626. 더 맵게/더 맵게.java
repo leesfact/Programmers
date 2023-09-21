c3553db648232c63ebe215362003fece07a1d4f0
leesfact <121993153+leesfact@users.noreply.github.com>
2023-09-21 21:35:23 +0900

    [level 2] Title: 더 맵게, Time: 1528.91 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -1,29 +1,17 @@
 import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
-       int mixCount = 0;
-        
-        PriorityQueue<Integer> scovilleQueue = new PriorityQueue<>();
-        
-        for(int a : scoville) {
-        	scovilleQueue.offer(a);
-        }
-        
-       
-        while(scovilleQueue.size() > 1 && scovilleQueue.peek() < K) {
-        	int first = scovilleQueue.poll();
-        	int second = scovilleQueue.poll();
-        	
-        	int mixScoville = first + (second * 2);
-        	scovilleQueue.offer(mixScoville);
-        	mixCount++;
-        }
-        System.out.println(scovilleQueue.peek());
-        if (scovilleQueue.peek() < K) {
-            return -1;
-        }
-        
-        
-        return mixCount;
+       PriorityQueue<Integer> pqScov = new PriorityQueue<>();
+         for (int s: scoville) {
+             pqScov.add(s);
+         }
+
+         int cnt = 0;
+         while (pqScov.size() > 1 && pqScov.peek() < K) {
+             pqScov.add(pqScov.remove() + pqScov.remove() * 2);
+             cnt++;
+         }
+
+         return pqScov.peek() >= K ? cnt : -1;
     }
 }
