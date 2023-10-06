d318d8d38bf6ded15b72309efe629a7b8d9958ef
leesfact <121993153+leesfact@users.noreply.github.com>
2023-10-06 22:27:37 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.07 ms, Memory: 52 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int mod = 1000000007;
+        
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i = 3; i <= n; i++) {
+        	dp[i] = (dp[i-1] + dp[i-2]) % mod;
+        }
+       
+      
+        return dp[n];
+    }
+}
