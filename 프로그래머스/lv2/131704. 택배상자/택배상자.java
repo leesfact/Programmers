af4bd5c88001f882f8c0958567a941bfe2bd3c6a
leesfact <121993153+leesfact@users.noreply.github.com>
2023-10-08 21:18:07 +0900

    [level 2] Title: 택배상자, Time: 148.17 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/lv2/131704. 택배상자/택배상자.java
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.java
@@ -1,30 +1,19 @@
 import java.util.*;
 class Solution {
     public int solution(int[] order) {
-       int[] priority = new int[order.length];
-		
-		for(int i = 0; i < order.length; i++) {
-			priority[order[i] - 1] = i;
-		}
-		
-		
+       int idx = 0;
 		Stack<Integer> stack = new Stack<>();
 		
-		int target = 0;
-		
-		for(int i = 0; i < priority.length; i++) {
-			if(priority[i] == target) {
-				target++;
-			}else {
-				stack.push(priority[i]);
-			}
-			while(!stack.isEmpty() && stack.peek() == target) {
-				stack.pop();
-				target++;
+		for(int i = 0; i < order.length; i++) {
+			stack.push(i+1);
+			while(!stack.isEmpty()) {
+				if(stack.peek() == order[idx]) {
+					stack.pop();
+					idx++;
+				}else break;
 			}
 		}
 		
-		
-		return target;
+		return idx;
     }
 }
