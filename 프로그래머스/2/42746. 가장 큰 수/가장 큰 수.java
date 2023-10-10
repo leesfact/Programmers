611df92f4ef521cecc954a9f9a8047bd1830708e
leesfact <121993153+leesfact@users.noreply.github.com>
2023-10-10 23:14:12 +0900

    [level 2] Title: 가장 큰 수, Time: 2.88 ms, Memory: 84.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        String[] strNumbers = new String[numbers.length];
+		
+		for(int i = 0; i < numbers.length; i++) {
+			strNumbers[i] = String.valueOf(numbers[i]);
+		}
+		
+		Arrays.sort(strNumbers, (o1, o2) -> (o2 + o1).compareTo(o1+o2));
+		
+		if(strNumbers[0].equals("0")) {
+			return "0";
+		}
+        
+        return String.join("", strNumbers);
+    }
+}
