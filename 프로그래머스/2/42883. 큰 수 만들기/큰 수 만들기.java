7c220e50e7fa5957dd5e4e384ab7ca305b146498
leesfact <121993153+leesfact@users.noreply.github.com>
2023-10-12 23:04:17 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.36 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public String solution(String number, int k) {
+        Stack<Character> stack = new Stack<>();
+		
+		for(char c : number.toCharArray()) {
+			while(!stack.isEmpty() && stack.peek() < c && k > 0) {
+				stack.pop();
+				k--;
+			}
+			stack.push(c);
+			
+		}
+		
+		while(k > 0) {
+            stack.pop();
+            k--;
+        }
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(char c : stack) {
+			sb.append(c);
+		}
+        
+        return sb.toString();
+    }
+}
