5e315e1d02b387eeb23034d2a2842c1e8c4c450a
leesfact <121993153+leesfact@users.noreply.github.com>
2023-11-01 18:28:29 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,21 +1,23 @@
 class Solution {
     public int solution(String name) {
         int answer = 0;
-        int nameLength = name.length();
-        int move = nameLength - 1;  // 최대로 이동할 수 있는 횟수 (왼쪽 혹은 오른쪽으로 모두 이동하는 경우)
-
-        for(int i = 0; i < nameLength; i++) {
-            int upDown = Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);  // 위로 이동하는 횟수와 아래로 이동하는 횟수 중 작은 값
-            answer += upDown;
-
-            int next = i + 1;
-            while(next < nameLength && name.charAt(next) == 'A') {
-                next++; 
-            }
-
-            move = Math.min(move, i + nameLength - next + Math.min(i, nameLength - next));  // 이
+        int len = name.length(); 
+        
+        int move = len - 1;
+        for (int i = 0; i < len; i++) {
+            char c = name.charAt(i);
+            answer += Math.min(c - 'A', 'Z' - c + 1);  
+            
+            int nextIdx = i + 1;
+        	
+        	while(nextIdx < len && name.charAt(nextIdx) == 'A') {
+        		nextIdx++;
+        	}
+        	move = Math.min(move, i + i + len - nextIdx);
+            move = Math.min(move, (len - nextIdx) * 2 + i);
+  
         }
-
+        
         answer += move;
         
         return answer;
