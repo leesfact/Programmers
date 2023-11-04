4e960fd65672655e70052d6d10b57bea39e574c6
leesfact <121993153+leesfact@users.noreply.github.com>
2023-11-04 15:24:40 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.58 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,42 +1,52 @@
 import java.util.*;
 class Solution {
     
-    static List<Integer> [] adjList;
-    static boolean[] visited;
-    public int solution(int n, int[][] wires) {
+    static Map<Integer, List<Integer>> adjMap ;
+	static boolean[] visited;
+	
+	public static int solution(int n, int[][] wires) {
         int answer = Integer.MAX_VALUE;
+        adjMap = new HashMap<>();
         
-        adjList = new ArrayList[n+1];
         
-        for(int i = 1; i <= n; i++){
-            adjList[i] = new ArrayList<>();
+        for(int i = 1; i <= n; i++) {
+        	adjMap.put(i, new ArrayList<>());
         }
         
-        for(int[] wire : wires){
+        for (int[] wire : wires) {
             int start = wire[0];
             int end = wire[1];
-            adjList[start].add(end);
-            adjList[end].add(start);
+            adjMap.get(start).add(end);
+            adjMap.get(end).add(start);
         }
         
-        for(int i = 0; i < wires.length; i++){
+        for (int i = 0; i < wires.length; i++) {
             visited = new boolean[n+1];
             int start = wires[i][0];
             int end = wires[i][1];
             
-            adjList[start].remove((Integer)end);
-            adjList[end].remove((Integer)start);
+            
+            
+            adjMap.get(start).remove((Integer) end);
+            adjMap.get(end).remove((Integer) start);
+            
+            
             
             int count1 = dfs(start);
             int count2 = dfs(end);
             
+           
+            
             answer = Math.min(answer, Math.abs(count1 - count2));
             
             
-            adjList[start].add(end);
-            adjList[end].add(start);
-        
+            adjMap.get(start).add(end);
+            adjMap.get(end).add(start);
+
+            
+            
         }
+       
         return answer;
     }
     
@@ -44,7 +54,7 @@ class Solution {
 		visited[node] = true;
 		int count = 1;
 		
-		for(int adj : adjList[node]) {
+		for(int adj : adjMap.get(node)) {
 			if(!visited[adj]) {
 				count += dfs(adj);
 			}
