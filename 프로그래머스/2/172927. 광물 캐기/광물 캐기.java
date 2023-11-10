7b8f1a5ebba230acf7c30db266dc3560cbcf0c2c
leesfact <121993153+leesfact@users.noreply.github.com>
2023-11-10 23:59:45 +0900

    [level 2] Title: 광물 캐기, Time: 381.07 ms, Memory: 92.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -0,0 +1,50 @@
+import java.util.*;
+class Solution {
+    HashMap<String, Integer> map ;
+    int[][] cost;
+    String[] minerals;
+    int minLen;
+    public int solution(int[] picks, String[] minerals) {
+        map = new HashMap<>();
+        int index = 0;
+        this.minerals = minerals;
+        minLen = Integer.MAX_VALUE;
+        map.put("diamond", index++);
+        map.put("iron", index++);
+        map.put("stone",index++);
+        cost = new int[][]{{1,1,1},{5,1,1},{25,5,1}};
+        int pickLen = 0;
+        for(int x : picks)pickLen+=x;
+        
+        dfs(new int[pickLen], 0, picks);
+        
+        return minLen;
+    }
+    public void dfs(int arr[], int L, int[] picks){
+        if(L == arr.length){
+            minLen = Math.min(minLen , getLen(arr));
+            return;
+        }
+        for(int i= 0; i< picks.length; i++){
+            if(picks[i]>0){
+                picks[i]--;
+                arr[L] = i;
+                dfs(arr, L+1, picks);
+                picks[i]++;
+            }
+        }
+    }
+    //곡괭이 종류info[], 곡괭이 개수 pick, map
+    public int getLen(int[] arr){
+        int ret = 0;
+        int mIndex = 0;
+        for(int x : arr){
+            int cnt = 5;
+            while(cnt-- > 0){
+                if(mIndex == minerals.length)return ret;
+                ret += cost[x][map.get(minerals[mIndex++])];
+            }
+        }
+        return ret;
+    }
+}
