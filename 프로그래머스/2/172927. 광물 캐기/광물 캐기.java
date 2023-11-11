117a3de821cb27f4d97ec77d386eab578a8b4da7
leesfact <121993153+leesfact@users.noreply.github.com>
2023-11-11 23:42:47 +0900

    [level 2] Title: 광물 캐기, Time: 1.63 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,50 +1,101 @@
 import java.util.*;
 class Solution {
-    HashMap<String, Integer> map ;
-    int[][] cost;
-    String[] minerals;
-    int minLen;
-    public int solution(int[] picks, String[] minerals) {
-        map = new HashMap<>();
-        int index = 0;
-        this.minerals = minerals;
-        minLen = Integer.MAX_VALUE;
-        map.put("diamond", index++);
-        map.put("iron", index++);
-        map.put("stone",index++);
-        cost = new int[][]{{1,1,1},{5,1,1},{25,5,1}};
-        int pickLen = 0;
-        for(int x : picks)pickLen+=x;
-        
-        dfs(new int[pickLen], 0, picks);
-        
-        return minLen;
-    }
-    public void dfs(int arr[], int L, int[] picks){
-        if(L == arr.length){
-            minLen = Math.min(minLen , getLen(arr));
-            return;
+    static final int[][] fatigueCosts = {
+			{1, 1, 1},
+			{5, 1, 1},
+			{25, 5, 1}
+	};
+	
+	static class MineralGroup {
+        int[] groupMinerals;
+
+        public MineralGroup(int[] groupMinerals) {
+            this.groupMinerals = groupMinerals;
         }
-        for(int i= 0; i< picks.length; i++){
-            if(picks[i]>0){
-                picks[i]--;
-                arr[L] = i;
-                dfs(arr, L+1, picks);
-                picks[i]++;
+        
+        public int fatigueWithPickaxe(int[][] fatigueCosts, int pickaxe) {
+            int fatigue = 0;
+            for (int i = 0; i < groupMinerals.length; i++) {
+                fatigue += groupMinerals[i] * fatigueCosts[pickaxe][i];
+            }
+            return fatigue;
+        }
+	}
+    
+	
+
+	public static int solution(int[] picks, String[] minerals) {
+		List<MineralGroup> groups = new ArrayList<>();
+        int totalFatigue = 0;
+        
+        int totalPicks = Arrays.stream(picks).sum();
+        int mineralsToProcess = Math.min(minerals.length, totalPicks * 5);
+        
+        
+        for (int i = 0; i < mineralsToProcess; i += 5) {
+            int[] groupMinerals = new int[3];
+            int groupEnd = Math.min(i + 5, minerals.length); 
+            for (int j = i; j < groupEnd; j++) {
+                groupMinerals[getMineralIndex(minerals[j])]++;
+            }
+            groups.add(new MineralGroup(groupMinerals));
+        }
+        
+      
+        groups.sort((g1, g2) -> Integer.compare(
+            g2.fatigueWithPickaxe(fatigueCosts, 2), 
+            g1.fatigueWithPickaxe(fatigueCosts, 2)
+        ));
+        
+       
+        for (MineralGroup group : groups) {
+        	
+        	if (totalPicks == 0) {
+                break;
+            }
+        	
+            int bestPickaxe = selectBestPickaxeForGroup(picks, group.groupMinerals);
+            
+            if (bestPickaxe == -1) {
+                break;
+            }
+            
+            for (int i = 0; i < group.groupMinerals.length; i++) {
+                totalFatigue += group.groupMinerals[i] * fatigueCosts[bestPickaxe][i];
+            }
+            picks[bestPickaxe]--;
+        }
+        System.out.println(totalFatigue);
+        return totalFatigue;
+    }
+
+    private static int selectBestPickaxeForGroup(int[] remainingPicks, int[] groupMinerals) {
+        int minGroupFatigue = Integer.MAX_VALUE;
+        int bestPickaxe = -1;
+
+        
+        for (int i = 0; i < remainingPicks.length; i++) {
+            if (remainingPicks[i] > 0) {
+                int fatigue = 0;
+                for (int j = 0; j < 3; j++) {
+                    fatigue += groupMinerals[j] * fatigueCosts[i][j];
+                }
+                if (fatigue < minGroupFatigue) {
+                    minGroupFatigue = fatigue;
+                    bestPickaxe = i;
+                }
             }
         }
+
+        return bestPickaxe;
     }
-    //곡괭이 종류info[], 곡괭이 개수 pick, map
-    public int getLen(int[] arr){
-        int ret = 0;
-        int mIndex = 0;
-        for(int x : arr){
-            int cnt = 5;
-            while(cnt-- > 0){
-                if(mIndex == minerals.length)return ret;
-                ret += cost[x][map.get(minerals[mIndex++])];
-            }
+
+    private static int getMineralIndex(String mineral) {
+        switch (mineral) {
+            case "diamond": return 0;
+            case "iron":    return 1;
+            case "stone":   return 2;
+            default:        return -1;
         }
-        return ret;
     }
 }
