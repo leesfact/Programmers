5f9146600f0fee9f36d9297c3402fdd7e58ab9a8
leesfact <121993153+leesfact@users.noreply.github.com>
2023-11-16 21:23:10 +0900

    [level 2] Title: N-Queen, Time: 189.45 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,54 +1,36 @@
 import java.util.*;
 class Solution {
-    public static boolean isSafe(char[][] board, int row, int col, int n) {
-		
-		for(int i = 0; i < col; i++) {
-			if(board[row][i] == 'Q') {
-				return false;
-			}
-		}
-		for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
-	        if (board[i][j] == 'Q') {
-	            return false;
-	        }
-	    }
-
-	
-	    for (int i = row, j = col; j >= 0 && i < n; i++, j--) {
-	        if (board[i][j] == 'Q') {
-	            return false;
-	        }
-	    }
-
-	    return true;
-	}
-	
-	public static int solveNQUtil(char[][] board, int col, int n) {
-		 if (col == n) {
-			 	return 1;
-	        }
-	        int count = 0;
-	        for (int i = 0; i < n; i++) {
-	            if (isSafe(board, i, col, n)) {
-	                board[i][col] = 'Q';
-	                count += solveNQUtil(board, col + 1, n);
-	                board[i][col] = '.'; 
-	            }
-	        }
-	        
-	        return count;
-	}
-	
-	public static int solution(int n) {
-		
-		char[][] board = new char[n][n];
-		
-		for(int i = 0; i < n; i++) {
-			Arrays.fill(board[i], '.');
-		}
-		
-        int answer = solveNQUtil(board, 0, n);
-        System.out.println(answer);
+    
+    static int[] board;
+    static int answer;
+    public int solution(int n) {
+        
+        board = new int[n];
+        
+        backTracking(0,n);
         return answer;
+    }
+    
+    public static void backTracking(int depth, int n){
+        if(depth == n){
+            answer++;
+            return;
+        }
+        
+        for(int i = 0; i < n; i++){
+            board[depth] = i;
+            if(valid(depth)){
+                backTracking(depth + 1, n);
+            }
+        }
+        
+    }
+    
+    public static boolean valid(int i){
+        for(int j = 0; j < i; j++){
+            if(board[i] == board[j]) return false;
+            if (Math.abs(i - j) == Math.abs(board[i] - board[j])) return false;
+        }
+        return true;
     }
 }
