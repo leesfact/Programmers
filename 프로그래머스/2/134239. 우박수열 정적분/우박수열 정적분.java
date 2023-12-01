4e2ca0daba41b13db89970cfcd415f93d9fee622
leesfact <121993153+leesfact@users.noreply.github.com>
2023-12-01 19:02:20 +0900

    [level 2] Title: 우박수열 정적분, Time: 7.21 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
@@ -1,63 +1,32 @@
 import java.util.*;
 class Solution {
-    static List<Double> areas;
-	static List<Double> rangeWidth;
-	
-	public static void calculateArea(List<int[]> list) {
-		for(int i = 0; i < list.size() - 1; i++) {
-			double square = Math.min(list.get(i)[1], list.get(i+1)[1]);
-			double triangle = Math.abs(((double)list.get(i+1)[1] - (double)list.get(i)[1]) / 2);
-			areas.add(square+triangle);
-			
-		}
-		
-	}
-	public static void getRange(List<Double> range,int x1, int x2) {
-		
-		double totalArea = 0;
-		
-		if(x2 < x1) totalArea = -1.0;
-		else {
-			for(int i = x1; i < x2; i++) {
-				totalArea += range.get(i);
-			}			
-		}
-	
-		rangeWidth.add(totalArea);
-	}
-	public static double[] solution(int k, int[][] ranges) {
-        
-		areas = new ArrayList<>();
-		rangeWidth = new ArrayList<>();
-        int hailNumber = k;
-        int n = 0;
-        ArrayList<int[]> list = new ArrayList<>();
-        list.add(new int[] {n, hailNumber});
-        while(hailNumber != 1) {
-        	n++;
-        	if(hailNumber % 2 == 0) {
-        		hailNumber /= 2;
-        	}else {
-        		hailNumber = (hailNumber * 3) + 1;
-        	}
-        	list.add(new int[] {n,hailNumber});
-        	
-        	
+    public double[] solution(int k, int[][] ranges) {
+
+        List<Integer> collatz = new ArrayList<>();
+        while(k > 1) {
+            collatz.add(k);
+            if(k % 2 == 0) k /= 2;
+            else k = k*3 +1;
         }
-        calculateArea(list);
-        
-        
-        for(int[] range : ranges) {
-        	int a = range[0];
-        	int b = n + range[1];
-        	getRange(areas,a,b);
-        }
+        collatz.add(1);
+
         double[] answer = new double[ranges.length];
-        for(int i = 0; i < rangeWidth.size(); i++) {
-        	answer[i] = rangeWidth.get(i);
+        for(int get = 0 ; get < ranges.length ; get++) {
+            double area = 0;
+            int goalX = collatz.size() + ranges[get][1] - 1;
+
+            
+            if(ranges[get][0] > goalX) {
+                answer[get] = -1;
+                continue;
+            }
+
+            for(int x = ranges[get][0] ; x < goalX ; x++) {
+                area += (collatz.get(x)+collatz.get(x+1))/2.0D;
+            }
+            answer[get] = area;
         }
-        
-        System.out.println(Arrays.toString(answer));
+
         return answer;
     }
 }
