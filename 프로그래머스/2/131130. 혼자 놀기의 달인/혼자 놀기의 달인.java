08e3ce391ef86ce218f5bfe7085e6191b48b7793
leesfact <121993153+leesfact@users.noreply.github.com>
2023-12-03 22:30:45 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.72 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -1,49 +1,21 @@
 import java.util.*;
 class Solution {
-    static int[] box;
-	static ArrayList<Integer> groupList = new ArrayList<>();
-	
-	public static void dfs(int index) {
-		
-		
-	
-		int cnt = 0;
-		while(box[index] != -1) {
-			int nextIndex = box[index];
-			box[index] = -1;
-			index = nextIndex;
-			cnt++;
-		}
-		if(cnt > 0) {
-			groupList.add(cnt);			
-		}
-		
-		
-		
-	}
-	
-	public static int solution(int[] cards) {
-		
-		int N = cards.length;
-		box = new int[N+1];
-		
-		
-		for(int i = 1; i <= N; i++) {
-			box[i] = cards[i-1];
-		}
-		
-		for(int i = 1; i <= N; i++) {
-			if(box[i] != -1) {
-				dfs(i);					
-			}
-		}
-			
-		
-        Collections.sort(groupList, Collections.reverseOrder());
-        
-        if(groupList.size() < 2) return 0;
-        int answer = groupList.get(0) * groupList.get(1);
-        
-        return answer;
+    public int solution(int[] cards) {
+        int n = cards.length;
+        boolean[] visited = new boolean[n];
+        List<Integer> groups = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int now = i;
+            int cnt = 0;
+            while (!visited[now]) {
+                cnt++;
+                visited[now] = true;
+                now = cards[now] - 1;
+            }
+            groups.add(cnt);
+        }
+        Collections.sort(groups, Comparator.reverseOrder());
+        return (groups.size() == 1) ? 0 : groups.get(0) * groups.get(1);
     }
 }
