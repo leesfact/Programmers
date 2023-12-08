7ee2ab9f652fbcc32b489bbd502a2c44055e4f95
leesfact <121993153+leesfact@users.noreply.github.com>
2023-12-08 18:13:40 +0900

    [level 2] Title: 숫자 블록, Time: 209.96 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
@@ -1,26 +1,30 @@
 class Solution {
 public static int[] solution(long begin, long end) {
 		int[] answer = new int[(int)(end - begin) + 1];
-        
-		for (int i = 0; i < answer.length; i++) {
-			long blockNumber = begin + i;
-			
-			
-			for (long j = 2; j * j <= blockNumber; j++) {
-				if (blockNumber % j == 0) {
-					if(blockNumber / j > 10000000) {
-						answer[i] = (int) j;
-						continue;
-					}else {
-						answer[i] = (int) (blockNumber / j);
-					}
-					break;
-				}
-			}
-			if(answer[i] == 0) answer[i] = 1;		
-		}
-		if(begin == 1) answer[0] = 0;
-		
-		return answer;
+
+	        for (int i = 0; i < answer.length; i++) {
+	            long blockNumber = begin + i;
+
+	            if (blockNumber == 1) {
+	                answer[i] = 0; 
+	                continue;
+	            }
+
+	            answer[i] = 1; 
+
+	            for (long j = 2; j * j <= blockNumber; j++) {
+	                if (blockNumber % j == 0) {
+	                    long divisor = blockNumber / j;
+	                    if (divisor <= 10000000) {
+	                        answer[i] = (int)divisor; 
+	                        break;
+	                    }else {
+	                    	answer[i] = (int)j;
+	                    }
+	                }
+	            }
+	        }
+
+	        return answer;
 	}
 }
