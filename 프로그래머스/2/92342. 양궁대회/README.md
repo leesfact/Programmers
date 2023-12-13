c28d23f92ecb17ba7e73b1205becefce41b245dc
leesfact <121993153+leesfact@users.noreply.github.com>
2023-12-13 18:32:21 +0900

    [level 2] Title: 양궁대회, Time: 5.23 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/README.md
+++ 프로그래머스/2/92342. 양궁대회/README.md
@@ -1,10 +1,10 @@
 # [level 2] 양궁대회 - 92342 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342) 
 
 ### 성능 요약
 
-메모리: 82.6 MB, 시간: 1.06 ms
+메모리: 79.8 MB, 시간: 5.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 2일 19:11:59
+2023년 12월 3일 18:32:19
 
 ### 문제 설명
 
--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,74 +1,56 @@
 class Solution {
     
-    static int MAX = 11;
-	
+    
 	static int[] bestRyanList;
-	static int bestScoreDiff = 0;
+	static int[] ryanList;
+	static int MAX = 11;
+	static int bestScoreDiff = Integer.MIN_VALUE;
 	
-	public static void calculateScore(int[] info, int[] ryanList) {
-		int apeachScore = 0;
-	    int ryanScore = 0;
-	        
-        for(int i = 0; i < MAX; i++) {
-            if(info[i] == 0 && ryanList[i] == 0) continue;
-            
-            if(info[i] >= ryanList[i]) apeachScore += (10 - i);
-            else ryanScore += (10 - i);
-        }
+	public static int[] solution(int n, int[] info) {
         
-        int scoreDiff = ryanScore - apeachScore;
-        if(scoreDiff > bestScoreDiff) {
-            bestScoreDiff = scoreDiff;
-            bestRyanList = ryanList.clone();
-        } else if (scoreDiff == bestScoreDiff) {
-            for(int i = MAX - 1; i >= 0; i--) {
-                if(bestRyanList[i] != ryanList[i]) {
-                    if(ryanList[i] > bestRyanList[i]) {
-                        bestRyanList = ryanList.clone();
-                    }
-                    break;
-                }
-            }
-        }
+		ryanList = new int[MAX];
+		bestRyanList = new int[MAX];
 		
-	}
+		backtracking(0,n,info);
+		
+		
+		
+		if(bestScoreDiff <= 0) return new int[] {-1};
+        else return bestRyanList;
+  
+    }
 	
-	public static void search(int n , int[] info, int[] ryanList, int index) {
-		if(n < 0) return; // 남은 화살이 없다면 더 이상 진행할 수 없
+	public static void backtracking(int depth, int n, int[] info) {
 		
-		if(index == MAX) { // 모든 점대를 고려했다면,
-			ryanList[MAX - 1] += n; // 남은 모든 화살을 가장 낮은 점수대에 할
-			calculateScore(info, ryanList); // 점수를 계
-			ryanList[MAX - 1] -= n; // 백트래킹
+		
+		if(depth == n) {
+			int diff = score(info);
+			if(diff >= bestScoreDiff) {
+				bestScoreDiff = diff;
+				bestRyanList = ryanList.clone();
+			}
 			return;
 		}
 		
-		search(n, info, ryanList, index +1); // 현재 점수대에 화살을 쏘지않고 다음 점수대 고
-		 
-		if(n > info[index]) { // 현재 점수대에 어피치보다 하나 더 많은 화살을 쏠 수 있다
-			ryanList[index] = info[index] + 1; // 현재 점수대에 화살을 할당
-			search(n - ryanList[index], info, ryanList, index + 1); // 할당 후 남은 화살로 다음 점수대를 고려
-			ryanList[index] = 0; // 백트래킹
+		for(int i = 0; i < info.length && ryanList[i] <= info[i]; i++) {
+			ryanList[i] += 1;
+			backtracking(depth + 1, n, info);
+			ryanList[i] -=1;
 		}
-		
 	}
 	
-	public static int[] solution(int n, int[] info) {
-        bestRyanList = new int[MAX];
-        bestScoreDiff = 0;
-        
-        int[] ryanList = new int[MAX];
-        
-        search(n, info, ryanList, 0);
-        
-        
-        
-       
-        
-        if(bestScoreDiff == 0) return new int[] {-1};
-        else return bestRyanList;
-        
-        
-  
-    }
+	public static int score(int[] info) {
+		int apeachScore = 0, ryanScore = 0;
+		
+		for(int i = 0; i < ryanList.length; i++) {
+			if(info[i] == 0 && ryanList[i] == 0) continue;
+			if(info[i] >= ryanList[i]) apeachScore += (10 - i);
+			else ryanScore += (10 - i);
+		}
+		
+		
+		int diff = ryanScore - apeachScore;
+    	if(diff <= 0) return -1;
+    	return diff;
+	}
 }
