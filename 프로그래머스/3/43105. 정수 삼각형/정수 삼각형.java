dd4e4d2491602cb98edaed4c28bee2713146a274
leesfact <121993153+leesfact@users.noreply.github.com>
2023-12-22 22:53:31 +0900

    [level 3] Title: 정수 삼각형, Time: 7.84 ms, Memory: 61 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,27 +1,14 @@
+import java.util.*;
 class Solution {
     public static int solution(int[][] triangle) {
 		
-		int n = triangle.length;
-		int[][] dp = new int[n][n];
-		dp[0][0] = triangle[0][0];
-		
-		
-		for(int i = 1; i < n; i++) {
-			
-			dp[i][0] = dp[i - 1][0] + triangle[i][0];
-	        dp[i][i] = dp[i - 1][i - 1] + triangle[i][i];
-	
-	        for (int j = 1; j < i; j++) {
-	            dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
-	        }
-		}
-		
-		
-	    int maxSum = dp[n - 1][0];
-	    for (int i = 1; i < n; i++) {
-	        maxSum = Math.max(maxSum, dp[n - 1][i]);
-	    }
+		 for (int i = 1; i < triangle.length; i++) {
+            triangle[i][0] += triangle[i-1][0];
+            triangle[i][i] += triangle[i-1][i-1];
+            for (int j = 1; j < i; j++) 
+                triangle[i][j] += Math.max(triangle[i-1][j-1], triangle[i-1][j]);
+        }
 
-	    return maxSum;
+        return Arrays.stream(triangle[triangle.length-1]).max().getAsInt();
 	}
 }
