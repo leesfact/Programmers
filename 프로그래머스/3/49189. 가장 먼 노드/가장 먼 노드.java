41894c8efaa8ea9339d1fc35425840a264fbf498
leesfact <121993153+leesfact@users.noreply.github.com>
2023-12-30 00:06:41 +0900

    [level 3] Title: 가장 먼 노드, Time: 45.56 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,6 +1,6 @@
 import java.util.*;
 class Solution {
-    static ArrayList<Integer>[] graph;
+    static Map<Integer, List<Integer>> graph;
 	static boolean[] visited;
 	static int[] distance;
 	
@@ -14,7 +14,7 @@ class Solution {
 		while(!q.isEmpty()) {
 			int currrent = q.poll();
 			
-			for(int next : graph[currrent]) {
+			for(int next : graph.get(currrent)) {
 				if(!visited[next]) {
 					q.add(next);
 					visited[next] = true;
@@ -26,22 +26,20 @@ class Solution {
 	
 	
 	public static int solution(int n, int[][] edge) {
-		graph = new ArrayList[n+1];
+		graph = new HashMap<>();
 		
-		for(int i = 0; i <= n; i++) {
-			graph[i] = new ArrayList<>();
+		for (int[] link : edge) {
+			int x = link[0];
+			int y = link[1];
+			
+			graph.computeIfAbsent(x, k -> new ArrayList<>()).add(y);
+			graph.computeIfAbsent(y, k -> new ArrayList<>()).add(x);
 		}
+		
 		
 		visited = new boolean[n+1];
 		distance = new int[n+1];
 		
-		for (int[] link : edge) {
-            int x = link[0];
-            int y = link[1];
-
-            graph[x].add(y);
-            graph[y].add(x);
-        }
 		
 		bfs(1);
 		
@@ -59,6 +57,7 @@ class Solution {
         		count++;
         	}
         }
+        
         
         return count;
     }
