488272a1660378d3a509e465c3de044c6d4e8102
leesfact <121993153+leesfact@users.noreply.github.com>
2024-01-07 21:25:50 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 25.47 ms, Memory: 58.1 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -1,32 +1,35 @@
 class Solution
 {
-    public static boolean isPalindrome(String s, int start, int end) {
+    public int solution(String s)
+    {
+       int n = s.length();
+		boolean[][] dp = new boolean[n][n];
+		int maxLength = 1;
 		
-		while(start < end) {
-			if(s.charAt(start) != s.charAt(end)){
-				return false;
-			}
-			
-			start++;
-			end--;
+		
+		// 한 글자는 모두 팰린드롬
+		for(int i = 0; i < n; i++) {
+			dp[i][i] = true;
 		}
 		
-		return true;
-	}
-	
-	
-	
-	public static int solution(String s) {
-      
-		int maxLength = 0;
-		
-		for(int start = 0; start < s.length(); start++) {
-			for(int end = s.length() - 1; end >= start; end--) {
-				if(s.charAt(start) == s.charAt(end) && isPalindrome(s, start, end)) {
-					maxLength = Math.max(maxLength, end - start + 1);
-				}
+		// 두 글자 팰린드롬
+		for(int i = 0; i < n - 1; i++) {
+			if(s.charAt(i) == s.charAt(i+1)) {
+				dp[i][i + 1] = true;
+				maxLength = 2;
 			}
-			
+		}
+		
+		// 길이가 3 이상인 팰린드롬 확인
+		
+		for(int len = 3; len <= n; len++) {
+			for(int start = 0; start <= n - len; start++) {
+				int end = start + len - 1;
+				if (s.charAt(start) == s.charAt(end) && dp[start + 1][end - 1]) {
+                    dp[start][end] = true;
+                    maxLength = len;
+                }
+			}
 		}
 		
 		return maxLength;
