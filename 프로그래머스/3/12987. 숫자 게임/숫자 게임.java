78d8f52c52f66eaeaa670fa8bb94d1f45d40e991
leesfact <121993153+leesfact@users.noreply.github.com>
2024-01-09 23:34:01 +0900

    [level 3] Title: 숫자 게임, Time: 583.12 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -2,16 +2,22 @@ import java.util.*;
 class Solution {
     public int solution(int[] A, int[] B) {
         int answer = 0;
-
         Arrays.sort(A);
         Arrays.sort(B);
-        for (int i = A.length - 1, j = B.length - 1; i >= 0; i--) {
-            if(A[i] < B[j]) {
-                answer++;
-                j--;
-            }
+        List<Integer> listB = new ArrayList<Integer>();
+        for(int i : B) {
+            listB.add(i);
         }
 
+        for(int i = 0; i < A.length; i++) {
+            for(int j = 0; j < listB.size(); j++) {
+                if(listB.get(j) > A[i]) {
+                    listB.remove(j);
+                    answer++;
+                    break;
+                }
+            }
+        }
         return answer;
     }
 }
