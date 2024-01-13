bd58095e437b222936bc83597d372f3d9a7bb5bd
leesfact <121993153+leesfact@users.noreply.github.com>
2024-01-13 22:15:36 +0900

    [level Lv.3] Title: 가장 긴 팰린드롬, Time: 0.14 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/Lv.3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/Lv.3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -1,38 +1,35 @@
 class Solution
 {
-    public int solution(String str)
+    public int solution(String s)
     {
-       int answer = 1; 
-
-        int start = 0;
-        int len = str.length();
-
-        int low, high;
-
-        for(int i = 1; i < len; ++i) {
-            low = i - 1;
-            high = i;
-            while(low >= 0 && high < len && str.charAt(low) == str.charAt(high)) {
-                if (high - low + 1 > answer) {
-                    start = low;
-                    answer = high - low + 1;
+       int answer = 0;
+        for(int start = 0, end = s.length(); start < end; start++) {
+            int startIndex = start;
+            int endIndex = end - 1;
+            int maxCount = end - start;
+            int searchLastIndex = -1;
+            if(answer >= maxCount) break;
+            while(endIndex > startIndex) {
+                char ch1 = s.charAt(startIndex);
+                char ch2 = s.charAt(endIndex);
+                if(ch1 == ch2) {
+                    if(searchLastIndex == -1)
+                        searchLastIndex = endIndex;
+                    startIndex++;
+                    endIndex--;
+                } else {
+                    if(searchLastIndex == -1) 
+                        endIndex--;
+                    else {
+                        startIndex = start;
+                        endIndex = searchLastIndex - 1;
+                        searchLastIndex = -1;
+                    }
+                    maxCount--;
                 }
-                --low;
-                ++high;
             }
-
-            low = i - 1;
-            high = i + 1;
-            while(low >= 0 && high < len && str.charAt(low) == str.charAt(high)) {
-                if (high - low + 1 > answer) {
-                    start = low;
-                    answer = high - low + 1;
-                }
-                --low;
-                ++high;
-            }
+            answer = answer < maxCount ? maxCount : answer;
         }
-
         return answer;
     }
 }
