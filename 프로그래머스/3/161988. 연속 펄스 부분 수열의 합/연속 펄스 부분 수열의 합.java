649bd17b6112da689bf0daffb331adfe238cfb16
leesfact <121993153+leesfact@users.noreply.github.com>
2024-01-22 15:11:43 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 12.61 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -3,22 +3,24 @@ class Solution {
     public static long solution(int[] sequence) {
         long answer = Math.max(findMaxPulseSum(sequence,1), findMaxPulseSum(sequence,-1));
         
-        
         return answer;
     }
 	
 	public static long findMaxPulseSum(int[] sequence,int startPulse) {
 		
-		long maxSoFar = startPulse * sequence[0];
-		long maxEndingHere = startPulse * sequence[0];
+		long maxSoFar = 0;
+		long maxEndingHere = 0;
 		int pulse = startPulse;
 		
-		for(int i = 1; i < sequence.length; i++) {
+		for(int i = 0; i < sequence.length; i++) {
 			pulse *= -1;
 			
 			long currentVal = pulse * sequence[i];
-			maxEndingHere = Math.max(currentVal, maxEndingHere + currentVal);
-            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+			maxEndingHere += currentVal;
+			
+			if(maxEndingHere < 0) maxEndingHere = 0;
+			
+			if(maxSoFar < maxEndingHere) maxSoFar = maxEndingHere;
 		}
 		
 		return maxSoFar;
