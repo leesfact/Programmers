a6d118fb4e1b163946b63f08423832de081ebd34
leesfact <121993153+leesfact@users.noreply.github.com>
2024-01-28 22:59:28 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.11 ms, Memory: 67 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,67 +1,59 @@
 import java.util.*;
 class Solution {
-    static int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; 
+    static int[][] DIRS = {{-1,0},{1,0},{0,-1},{0,1}};
     static int[][][] cost; 
     static int N;
-    static final int STRAIGHT = 100;
-    static final int CORNER = 500;
-	
-    public static void bfs(int[][] board) {
-        Queue<int[]> queue = new LinkedList<>();
-        
-        queue.offer(new int[]{0, 0, -1, 0});
+    static final int STRAIGHT_COST = 100;
+    static final int CORNER_COST = 500;
+    
+    public static void dfs(int[][] board, int y, int x, int prevDir, int accCost) {
+        if (y == N - 1 && x == N - 1) {
+            cost[y][x][prevDir] = Math.min(cost[y][x][prevDir], accCost);
+            return;
+        }
 
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int y = current[0];
-            int x = current[1];
-            int prevDir = current[2];
-            int accCost = current[3];
+        for (int i = 0; i < 4; i++) {
+            int newY = y + DIRS[i][0];
+            int newX = x + DIRS[i][1];
 
-            for (int i = 0; i < 4; i++) {
-                int newY = y + dirs[i][0];
-                int newX = x + dirs[i][1];
+            if (newY >= 0 && newY < N && newX >= 0 && newX < N && board[newY][newX] == 0) {
+                int newCost = accCost + STRAIGHT_COST;
+                
+                
+                if (prevDir != -1 && prevDir != i) {
+                    newCost += CORNER_COST;
+                }
 
-                if (newY >= 0 && newY < N && newX >= 0 && newX < N && board[newY][newX] == 0) {
-                    int newCost = accCost + STRAIGHT;
-
-                    
-                    if (prevDir != -1 && prevDir != i) {
-                        newCost += CORNER;
-                    }
-
-                    if (newCost < cost[newY][newX][i]) {
-                        cost[newY][newX][i] = newCost;
-                        queue.offer(new int[]{newY, newX, i, newCost});
-                    }
+                if (newCost < cost[newY][newX][i]) {
+                    cost[newY][newX][i] = newCost;
+                    dfs(board, newY, newX, i, newCost);
                 }
             }
         }
     }
-	
-	
-	
+    
     public static int solution(int[][] board) {
         N = board.length;
-        cost = new int[N][N][4];
+        cost = new int[N][N][4]; 
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 Arrays.fill(cost[i][j], Integer.MAX_VALUE);
             }
         }
-
         
         for (int i = 0; i < 4; i++) {
-            cost[0][0][i] = 0;
+            cost[0][0][i] = 0; 
         }
 
-        bfs(board);
+        dfs(board, 0, 0, -1, 0);
 
         
         int minCost = Integer.MAX_VALUE;
         for (int i = 0; i < 4; i++) {
-            minCost = Math.min(minCost, cost[N - 1][N - 1][i]);
+            if (cost[N-1][N-1][i] < minCost) {
+                minCost = cost[N-1][N-1][i];
+            }
         }
 
         return minCost;
