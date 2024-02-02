7cff8120f8c2ae69778c9ab4088e647b31be42f2
leesfact <121993153+leesfact@users.noreply.github.com>
2024-02-02 16:20:16 +0900

    [level 3] Title: 풍선 터트리기, Time: 251.66 ms, Memory: 207 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -1,32 +1,20 @@
+import java.util.*;
 class Solution {
     public int solution(int[] a) {
-        int answer = 0;
-        int length = a.length;
+        int leftMin = Integer.MAX_VALUE;
+        int rightMin = Integer.MAX_VALUE;
+        Set<Integer> set = new HashSet<>();
         
-        int[] leftMin = new int[length];
-        int[] rightMin = new int[length];
-        
-
-        leftMin[0] = a[0];
-        rightMin[length-1] = a[length-1];
-        
-        for(int i = 1; i < length; i++) {
-        	leftMin[i] = Math.min(leftMin[i-1], a[i]);
+        for(int i = 0; i < a.length; i++) {
+        	leftMin = Math.min(leftMin, a[i]);
+        	rightMin = Math.min(rightMin, a[a.length-1-i]);
+        	set.add(leftMin);
+        	set.add(rightMin);
         }
+        
        
         
         
-        for(int i = length - 2; i >= 0; i-- ) {
-        	rightMin[i] = Math.min(rightMin[i+1], a[i]);
-        }
-       
-        
-        for(int i = 0; i < length; i++) {
-        	if (i == 0 || i == length - 1 || a[i] < leftMin[i - 1] || a[i] < rightMin[i + 1]) {
-                answer++;
-            }
-        }
-        
-        return answer;
+        return set.size();
     }
 }
