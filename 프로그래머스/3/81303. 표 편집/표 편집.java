2bdcbe0e1ab08deb6362ef04ee31104c3ce0076a
leesfact <121993153+leesfact@users.noreply.github.com>
2024-02-16 21:43:41 +0900

    [level 3] Title: 표 편집, Time: 853.38 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,76 +1,37 @@
 import java.util.*;
 class Solution {
-   static class Node{
-		int index;
-		Node prev, next;
-		boolean removed = false;
-		
-		public Node(int index) {
-			this.index = index;
-		}
-		
-		public Node delete() {
-			this.removed = true;
-			if(prev != null) prev.next = this.next;
-			if(next != null) next.prev = this.prev;
-			return next != null? next : prev;
-		}
-		
-		public void restore() {
-			this.removed = false;
-			if(prev != null) prev.next = this;
-			if(next != null) next.prev = this;
-		}
-	}
-	
-	public static String solution(int n, int k, String[] cmd) {
-		
-		Node[] nodes = new Node[n];
-		Stack<Node> deletedNodes = new Stack<>();
-		
-		for(int i = 0; i < n; i++) {
-			nodes[i] = new Node(i);
-		}
-		
-		for(int i = 1; i < n; i++) {
-			nodes[i - 1].next = nodes[i];
-			nodes[i].prev = nodes[i - 1];
-		}
-		
-		Node currnetIndex = nodes[k];
-		
-		for(String command : cmd) {
-			char shortcutKey = command.charAt(0);
-			int move = 0;
-			
-			if(command.length() > 1) {
-				move = Integer.parseInt(command.split(" ")[1]);
-			}
-			
-			switch(shortcutKey) {
-			case 'U' :
-				while(move-- > 0) currnetIndex = currnetIndex.prev;
-				break;
-			case 'D':
-				while(move-- > 0)currnetIndex = currnetIndex.next;
-				break;
-			case 'C':
-				deletedNodes.push(currnetIndex);
-				currnetIndex = currnetIndex.delete();
-				break;
-			case 'Z':
-				deletedNodes.pop().restore();
-				break;
-			}
-		}
-		StringBuilder sb = new StringBuilder("O".repeat(n));
-		while(!deletedNodes.isEmpty()) {
-			Node node = deletedNodes.pop();
-			if(node.removed) {
-				sb.setCharAt(node.index, 'X');
-			}
-		}
-        
+    public String solution(int n, int k, String[] cmd) {
+
+        int tablesize = n;
+        Stack<Integer> st = new Stack<>();
+
+        for (String command : cmd) {
+            String[] commandSplit = command.split(" ");
+            if (commandSplit[0].equals("U")) {
+                k -= Integer.parseInt(commandSplit[1]);
+                if (k < 0) k = 0;
+            } else if (commandSplit[0].equals("D")) {
+                k += Integer.parseInt(commandSplit[1]);
+                if (k > tablesize - 1) k = tablesize - 1;
+            } else if (commandSplit[0].equals("C")) {
+                st.push(k);
+                tablesize--;
+                if(k == tablesize) k--;
+            } else {
+                int ret = st.pop();
+                if(k>=ret) k++;
+                tablesize++;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < tablesize; i++) {
+            sb.append("O");
+        }
+        while(!st.empty()){
+            sb.insert(st.pop().intValue(),'X');
+        }
+       
         return sb.toString();
     }
 }
