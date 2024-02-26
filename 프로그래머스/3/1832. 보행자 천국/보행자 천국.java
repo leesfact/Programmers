f03fcf77bcf4b0c417d703562511f208e06968dd
leesfact <121993153+leesfact@users.noreply.github.com>
2024-02-26 21:43:16 +0900

    [level 3] Title: 보행자 천국, Time: 97.62 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
@@ -1,28 +1,24 @@
 class Solution {
-    static int MOD = 20170805;
-   
-	public static int solution(int m, int n, int[][] cityMap) {
-		
-		int[][] right = new int[m + 1][n + 1];
+    int MOD = 20170805;
+    public int solution(int m, int n, int[][] cityMap) {
+        int[][] right = new int[m + 1][n + 1];
         int[][] down = new int[m + 1][n + 1];
         
-        right[0][0] = 1;
-
+        right[1][1] = 1;
+        down[1][1] = 1;
         
-        for (int y = 0; y < m; y++) {
-            for (int x = 0; x < n; x++) {
-                if (cityMap[y][x] == 0) {
-                    right[y+1][x] += (right[y][x] + down[y][x]) % MOD;
-                    down[y][x+1] += (right[y][x] + down[y][x]) % MOD;
-                } else if (cityMap[y][x] == 2) {
-                    right[y+1][x] = right[y][x];
-                    down[y][x+1] = down[y][x];
-                }
-                
-            }
+        for(int y = 1; y <= m; y++) {
+        	for(int x = 1; x <= n; x++) {
+        		if(cityMap[y-1][x-1] == 0) {
+        			right[y][x] += (right[y][x-1] + down[y-1][x]) % MOD;
+        			down[y][x] += (right[y][x-1] + down[y-1][x]) % MOD;
+        		}else if(cityMap[y-1][x-1] == 2) {
+        			right[y][x] = right[y][x-1];
+        			down[y][x] = down[y-1][x];
+        		}
+        	}
         }
         
-        
-        return (right[m-1][n-1] + down[m-1][n-1]) % MOD;
+        return (right[m][n - 1] + down[m - 1][n]) % MOD;
     }
 }
