d97276ac732fbe8bf9b2b61ea4b46cae2fa5fdc3
leesfact <121993153+leesfact@users.noreply.github.com>
2024-02-27 22:02:51 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.09 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,61 +1,37 @@
 class Solution {
-    public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        int distance = Math.abs(r - x) + Math.abs(c - y);
-        if (distance > k) {
-            return "impossible";
+   static int[][] DIRS = {{1,0}, {0,-1}, {0,1}, {-1,0}};
+    static char[] dirChars = {'d', 'l', 'r', 'u'};
+    static String bestPath = null;
+
+    public static boolean dfs(int x, int y, int r, int c, int k, StringBuilder path, int n, int m) {
+        int remainingDistance = Math.abs(r - x) + Math.abs(c - y);
+
+        if (k < remainingDistance || (k - remainingDistance) % 2 != 0) return false;
+
+        if (x == r && y == c && k == 0) {
+            bestPath = path.toString();
+            return true; // 유효한 경로를 찾았으므로 true 반환
         }
 
-        if ((k - distance) % 2 != 0) {
-            return "impossible";
-        }
-
-        int down = Math.max(r - x, 0);
-        int left = Math.max(y - c, 0);
-        int right = Math.max(c - y, 0);
-        int up = Math.max(x - r, 0);
-        int pair = (k - distance) / 2;
-
-        StringBuilder answer = new StringBuilder();
-
-        for (int i = 0; i < k; i++) {
-            if ((down > 0 || pair > 0) && x < n) {
-                answer.append('d');
-                if (down > 0) {
-                    down--;
-                } else {
-                    pair--;
-                    up++;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + DIRS[i][0];
+            int ny = y + DIRS[i][1];
+            if (nx > 0 && nx <= n && ny > 0 && ny <= m) {
+                path.append(dirChars[i]);
+                if (dfs(nx, ny, r, c, k - 1, path, n, m)) {
+                    return true; // 하위 호출에서 유효한 경로를 찾았으므로 추가 탐색 중단
                 }
-                x++;
-            } else if ((left > 0 || pair > 0) && y > 1) {
-                answer.append('l');
-                if (left > 0) {
-                    left--;
-                } else {
-                    pair--;
-                    right++;
-                }
-                y--;
-            } else if ((right > 0 || pair > 0) && y < m) {
-                answer.append('r');
-                if (right > 0) {
-                    right--;
-                } else {
-                    pair--;
-                    left++;
-                }
-                y++;
-            } else { 
-                answer.append('u');
-                if (up > 0) {
-                    up--;
-                } else {
-                    pair--;
-                    down++;
-                }
-                x--;
+                path.deleteCharAt(path.length() - 1);
             }
         }
-        return answer.toString();
+        return false; // 이 지점에 도달했다면 유효한 경로를 찾지 못한 것이므로 false 반환
+    }
+
+    public static String solution(int n, int m, int x, int y, int r, int c, int k) {
+        StringBuilder path = new StringBuilder();
+        if (!dfs(x, y, r, c, k, path, n, m)) {
+            return "impossible";
+        }
+        return bestPath;
     }
 }
