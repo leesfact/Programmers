82ee4cebfb505935425d2d0b273ee179e550b91d
leesfact <121993153+leesfact@users.noreply.github.com>
2024-03-22 21:47:46 +0900

    [level 0] Title: 수 조작하기 1, Time: 1.84 ms, Memory: 71.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = n;
+       
+        for(int i = 0; i < control.length(); i++) {
+        	switch (control.charAt(i)) {
+			case 'w' :
+				answer += 1;
+				break;
+			case 's' :
+				answer -= 1;
+				break;
+			case 'd' :
+				answer += 10;
+				break;
+				
+			default:
+				answer -= 10;
+				break;
+			}
+        	
+        }
+        
+        return answer;
+    }
+}
