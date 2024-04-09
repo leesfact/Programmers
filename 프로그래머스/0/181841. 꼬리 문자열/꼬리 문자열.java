4ce59d373f4dbe65fc820dcf6d7060e8bf69e541
leesfact <121993153+leesfact@users.noreply.github.com>
2024-04-09 21:51:21 +0900

    [level 0] Title: 꼬리 문자열, Time: 1.45 ms, Memory: 71.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String[] str_list, String ex) {
+        String answer = "";
+        for(String str : str_list) {
+        	if(str.contains(ex)) {
+        		continue;
+        	}else {
+        		answer += str;
+        	}
+        }
+       
+        return answer;
+    }
+}
