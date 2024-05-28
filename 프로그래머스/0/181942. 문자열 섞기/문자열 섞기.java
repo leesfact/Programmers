b1dad24d0b31718b048af04e4ac258c973a705cd
leesfact <121993153+leesfact@users.noreply.github.com>
2024-05-28 22:55:18 +0900

    [level 0] Title: 문자열 섞기, Time: 11.64 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,10 +1,12 @@
 class Solution {
     public String solution(String str1, String str2) {
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < str1.length(); i++) {
-        	sb.append(str1.charAt(i)).append(str2.charAt(i));
+        String answer = "";
+
+        for(int i = 0; i < str1.length(); i++){
+            answer += str1.charAt(i);
+            answer += str2.charAt(i);
         }
         
-        return sb.toString();
+        return answer;
     }
 }
