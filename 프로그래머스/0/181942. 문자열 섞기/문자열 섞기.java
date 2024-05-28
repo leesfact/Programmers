49a75829d1fba9a2a667266db1d471c3a6419d60
leesfact <121993153+leesfact@users.noreply.github.com>
2024-05-28 22:53:30 +0900

    [level 0] Title: 문자열 섞기, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,22 +1,10 @@
 class Solution {
     public String solution(String str1, String str2) {
-        String answer = "";
-        char[] set = new char[str1.length() + str2.length()];
-        int index = 0;
-        for(int i = 0; i < set.length; i += 2) {
-        	set[i] = str1.charAt(index); 
-        	index++;
-        }
-        index = 0;
-        for(int i = 1; i < set.length; i += 2) {
-        	set[i] = str2.charAt(index); 
-        	index++;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < str1.length(); i++) {
+        	sb.append(str1.charAt(i)).append(str2.charAt(i));
         }
         
-        for(int i = 0; i < set.length; i++) {
-        	answer += set[i];
-        }
-        
-        return answer;
+        return sb.toString();
     }
 }
