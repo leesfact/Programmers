7dabe037476c1f02a4cb36c9125e5c5cb292aa5f
leesfact <121993153+leesfact@users.noreply.github.com>
2024-05-30 22:25:08 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 1.40 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] copyArr = arr.clone();
+        
+        for(int[] query : queries) {
+        	
+        	for(int i = query[0]; i <= query[1]; i++) {
+        		copyArr[i] += 1;
+        	}
+        }
+        
+        
+        return copyArr;
+    }
+}
