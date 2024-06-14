028adb595a800dbb2267b2ba60ea0f07f304f610
leesfact <121993153+leesfact@users.noreply.github.com>
2024-06-14 22:57:34 +0900

    [level 0] Title: 배열 만들기 4, Time: 5.96 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        
+        int cnt = 0;
+        while (cnt < arr.length) {
+        	if(stack.isEmpty()) stack.add(arr[cnt++]);
+        	else {
+        		if(stack.peek() < arr[cnt]) stack.add(arr[cnt++]);
+        		else stack.pop();
+        	}
+        	
+        }
+        int[] stk = new int[stack.size()];
+        for(int i = 0; i < stack.size(); i++) {
+        	stk[i] = stack.get(i);
+        }
+        
+        
+        return stk;
+    }
+}
