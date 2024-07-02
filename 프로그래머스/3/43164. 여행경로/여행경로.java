c17b6dba88250108056326e827f9c87116a4091f
leesfact <121993153+leesfact@users.noreply.github.com>
2024-07-02 15:33:25 +0900

    [level 3] Title: 여행경로, Time: 1.04 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,37 +1,6 @@
-import java.util.*;
 class Solution {
-    static Map<String, PriorityQueue<String>> graph;
-	static List<String> route;
-	static boolean found;
-	
-	public void dfs(String depart) {
-		PriorityQueue<String> arrival = graph.get(depart);
-		
-		
-		
-		while(arrival != null && !arrival.isEmpty()) {
-			dfs(arrival.poll());
-		}
-		
-		route.add(0, depart);
-		System.out.println(route);
-	}
-	
-	public String[] solution(String[][] tickets) {
-        graph = new HashMap<>();
-        route = new LinkedList<>();
-        
-        for(String[] ticket : tickets) {
-        	graph.putIfAbsent(ticket[0], new PriorityQueue<>());
-        	graph.get(ticket[0]).add(ticket[1]);
-        }
-        
-        
-        dfs("ICN");
-        
-       
-        
-        
-        return route.toArray(new String[0]);
+    public String[] solution(String[][] tickets) {
+        String[] answer = {};
+        return answer;
     }
 }
