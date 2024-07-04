d8be2c9a0d63bbf96794de552844fcaf96d03422
leesfact <121993153+leesfact@users.noreply.github.com>
2024-07-04 17:28:20 +0900

    [level 3] Title: 퍼즐 조각 채우기, Time: 0.63 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.java
+++ 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.java
@@ -1,142 +1,163 @@
 import java.util.*;
 class Point implements Comparable<Point> {
-    int x;
-    int y;
-
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    @Override
-    public int compareTo(Point o) {
-        if (this.x == o.x) {
-            return this.y - o.y;
-        }
-        return this.x - o.x;
-    }
-
+	int y, x;
+	public Point(int y, int x) {
+		this.y = y;
+		this.x = x;
+	}
+	@Override
+	public int compareTo(Point o) {
+		if(this.y == o.y) return this.x - o.x;
+		
+		return this.y - o.y;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return "(" + y + "," + x + ")";
+	}
+	
 }
+
 class Solution {
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
 
-    static int boardSize;
-    static ArrayList<ArrayList<Point>> empty = new ArrayList<>();
-    static ArrayList<ArrayList<Point>> block = new ArrayList<>();
-    static boolean[][] visited;
-
-    
-    public int solution(int[][] game_board, int[][] table) {
-        boardSize = game_board.length;
-        visited = new boolean[boardSize][boardSize];
-
-        for (int i = 0; i < boardSize; i++) {
-            for (int j = 0; j < boardSize; j++) {
-                if (game_board[i][j] == 0 && !visited[i][j]) {
-                    empty.add(check(game_board, i, j, 0));
-                }
-            }
-        }
-
-        for (int i = 0; i < boardSize; i++) {
-            Arrays.fill(visited[i], false);
-        }
-
-        for (int i = 0; i < boardSize; i++) {
-            for (int j = 0; j < boardSize; j++) {
-                if (table[i][j] == 1 && !visited[i][j])
-                    block.add(check(table, i, j, 1));
-            }
-        }
-
-        boolean[] visitedBoard = new boolean[empty.size()];
-        int answer = 0;
-
-        for (int i = 0; i < block.size(); i++) {
-            ArrayList<Point> blockCheck = block.get(i);
-
-            for (int j = 0; j < empty.size(); j++) {
-                ArrayList<Point> emptyCheck = empty.get(j);
-
-                if (emptyCheck.size() == blockCheck.size() && !visitedBoard[j]) {
-                    if (isRotate(emptyCheck, blockCheck)) {
-                        answer += blockCheck.size();
-                        visitedBoard[j] = true;
-                        break;
-                    }
-                }
-            }
-        }
-
-        return answer;
-    }
-    
-    public static ArrayList<Point> check(int[][] board, int x, int y, int type) {
-        Queue<Point> q = new LinkedList<>();
-        ArrayList<Point> result = new ArrayList<>();
-
-        q.add(new Point(x, y));
-        visited[x][y] = true;
-
-        result.add(new Point(0, 0));
-
-        while (!q.isEmpty()) {
-            Point cur = q.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
-
-                if (nx >= 0 && nx < boardSize && ny >= 0 && ny < boardSize) {
-                    if (!visited[nx][ny] && board[nx][ny] == type) {
-                        q.add(new Point(nx, ny));
-                        visited[nx][ny] = true;
-                        result.add(new Point(nx - x, ny - y));
-                    }
-                }
-            }
-        }
-
-        Collections.sort(result);
-        return result;
-    }
-
-    public static boolean isRotate(ArrayList<Point> empty, ArrayList<Point> block) {
-        for (int i = 0; i < 4; i++) {
-            int zeroX = block.get(0).x;
-            int zeroY = block.get(0).y;
-
-            for (int j = 0; j < block.size(); j++) {
-                block.get(j).x -= zeroX;
-                block.get(j).y -= zeroY;
-            }
-
-            boolean isCollect = true;
-
-            for (int j = 0; j < empty.size(); j++) {
-                Point emptyPoint = empty.get(j);
+	
+	static int[] dy = {-1, 1, 0, 0};
+	static int[] dx = {0, 0, -1, 1};
+	static int boardSize;
+	static ArrayList<ArrayList<Point>> empty = new ArrayList<>();
+	static ArrayList<ArrayList<Point>> block = new ArrayList<>();
+	static boolean[][] visited;
+	
+	
+	
+	public static boolean isRotate(ArrayList<Point> empty, ArrayList<Point> block) {
+		
+		for(int i = 0; i < 4; i++) {
+			
+			int zeroY = block.get(0).y;
+			int zeroX = block.get(0).x;
+			
+			
+			
+			for(int j = 0; j < block.size(); j++) {
+				block.get(j).y -= zeroY;
+				block.get(j).x -= zeroX;
+				
+			}
+			
+			boolean isCollect = true;
+			
+			for(int j = 0; j < empty.size(); j++) {
+				Point emptyPoint = empty.get(j);
                 Point blockPoint = block.get(j);
-
-                if (emptyPoint.x != blockPoint.x || emptyPoint.y != blockPoint.y) {
-                    isCollect = false;
-                    break;
+                
+                if(emptyPoint.y != blockPoint.y || emptyPoint.x != blockPoint.x) {
+                	isCollect = false;
+                	break;
                 }
-            }
-
-            if (isCollect) {
-                return true;
-            } else {
-                for (int j = 0; j < block.size(); j++) {
-                    int temp = block.get(j).x;
-                    block.get(j).x = block.get(j).y;
-                    block.get(j).y = -temp;
+			}
+			
+			if(isCollect) return true;
+			else {
+				for (int j = 0; j < block.size(); j++) {
+                    int originY = block.get(j).y;
+                    block.get(j).y = block.get(j).x;
+                    block.get(j).x = -originY;
                 }
 
                 Collections.sort(block);
-            }
-        }
+			}
+			
+			
+			
+		}
+		
+		
+		
+		return false;
+	}
+	
+	public static ArrayList<Point> extract (int[][] board, int y, int x, int type) {
+		
+		Queue<Point> queue = new LinkedList<>();
+		ArrayList<Point> bundle = new ArrayList<>();
+		visited[y][x] = true;
+		queue.add(new Point(y,x));
+		bundle.add(new Point(0,0));
+		
+		
+		while(!queue.isEmpty()) {
+			Point current = queue.poll();
+			
+			for(int i = 0; i < 4; i++) {
+				int ny = current.y + dy[i];
+				int nx = current.x + dx[i];
+				
+				if(ny >= 0 && ny < boardSize && nx >= 0 && nx < boardSize) {
+					if(board[ny][nx] == type && !visited[ny][nx]) {
+						queue.add(new Point(ny,nx));
+						visited[ny][nx] = true;
+						bundle.add(new Point(ny - y, nx - x));
+					}
+					
+				}
+			}
+		}
+		
+		Collections.sort(bundle);
+		
+		
+		
+		return bundle;
+				
+	}
+    
+   
 
-        return false;
+    public static int solution(int[][] game_board, int[][] table) {
+    	
+    	boardSize = game_board.length;
+    	
+    	visited = new boolean[boardSize][boardSize];
+    	
+    	for(int i = 0; i < boardSize; i++) {
+    		for(int j = 0; j < boardSize; j++) {
+    			if(game_board[i][j] == 0 && !visited[i][j]) {
+    				empty.add(extract(game_board,i,j,0));
+    			}
+    		}
+    	}
+    	
+    	
+    	visited = new boolean[boardSize][boardSize];
+    	for(int i = 0; i < boardSize; i++) {
+    		for(int j = 0; j < boardSize; j++) {
+    			if(table[i][j] == 1 && !visited[i][j]) {
+    				block.add(extract(table,i,j,1));
+    			}
+    		}
+    	}
+    	
+    	boolean[] filledBoard = new boolean[empty.size()];
+    	int filledCnt = 0;
+    	
+    	for(int i = 0; i < block.size(); i++) {
+    		ArrayList<Point> blockCheck = block.get(i);
+    		
+    		for(int j = 0; j < empty.size(); j++) {
+    			ArrayList<Point> emptyCheck = empty.get(j);
+    			if (emptyCheck.size() == blockCheck.size() && !filledBoard[j]) {
+    				if(isRotate(emptyCheck, blockCheck)) {
+    					filledCnt += blockCheck.size();
+    					filledBoard[j] = true;
+    					break;
+    				}    				
+    			}
+    		}
+    	}
+    	
+        return filledCnt;
     }
 }
